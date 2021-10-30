e4f30fbbfc3de6f19c145adc8750ec0367f49287
wenhaocs <19355821+wenhaocs@users.noreply.github.com>
2021-10-30 18:57:21 +0000

    get value for REPLY_OF

--- nebula_bench/scenarios/go.py
+++ nebula_bench/scenarios/go.py
@@ -4,26 +4,26 @@ from nebula_bench.common.base import BaseScenario
 
 class BaseGoScenario(BaseScenario):
     abstract = True
-    nGQL = "GO 1 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
-    csv_path = "social_network/dynamic/person.csv"
+    nGQL = "GO 1 STEP FROM {} OVER REPLY_OF YIELD properties($$).locationIP"
+    csv_path = "social_network/dynamic/comment.csv"
     csv_index = [0]
 
 
 class Go1Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 1 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 1 STEP FROM {} OVER REPLY_OF YIELD properties($$).locationIP"
 
 
 class Go2Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 2 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 2 STEP FROM {} OVER REPLY_OF YIELD properties($$).locationIP"
 
 
 class Go3Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 3 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 3 STEP FROM {} OVER REPLY_OF YIELD properties($$).locationIP"
 
 
 class Go5Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 5 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 5 STEP FROM {} OVER REPLY_OF YIELD properties($$).locationIP"
