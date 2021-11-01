e444fda9edf27f4ccc34abecb3a1dd08a82faf21
wenhaocs <19355821+wenhaocs@users.noreply.github.com>
2021-11-01 18:32:14 +0000

    yield edge property

--- nebula_bench/scenarios/go.py
+++ nebula_bench/scenarios/go.py
@@ -4,26 +4,26 @@ from nebula_bench.common.base import BaseScenario
 
 class BaseGoScenario(BaseScenario):
     abstract = True
-    nGQL = "GO 1 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 1 STEP FROM {} OVER KNOWS YIELD properties(EDGE).creationDate"
     csv_path = "social_network/dynamic/person.csv"
     csv_index = [0]
 
 
 class Go1Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 1 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 1 STEP FROM {} OVER KNOWS YIELD properties(EDGE).creationDate"
 
 
 class Go2Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 2 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 2 STEP FROM {} OVER KNOWS YIELD properties(EDGE).creationDate"
 
 
 class Go3Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 3 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 3 STEP FROM {} OVER KNOWS YIELD properties(EDGE).creationDate"
 
 
 class Go5Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 5 STEP FROM {} OVER KNOWS YIELD properties($$).birthday"
+    nGQL = "GO 5 STEP FROM {} OVER KNOWS YIELD properties(EDGE).creationDate"
